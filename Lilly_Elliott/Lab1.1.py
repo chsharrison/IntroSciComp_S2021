fc7ce3336b6d3adc777052799c1f8369b1b1e644
Lilly Elliott <77079153+lillyelliott@users.noreply.github.com>
2021-01-20 10:37:18 -0600

    Add files via upload

---
+++ Lilly_Elliott/Lab1.1.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+# Definition of savings and result
+savings = 100
+result = 100 * 1.10 ** 7
+
+# Fix the printout
+print("I started with $" + str(savings) + " and now have $" + str(result) + ". Awesome!")
+
+# Definition of pi_string
+pi_string = "3.1415926"
+
+# Convert pi_string into float: pi_float
+pi_float = float(pi_string)
+
+savings = 100
+
+# Create a variable growth_multiplier
+growth_multiplier = 1.1
+
+# Calculate result
+result = (savings*(growth_multiplier**7))
+
+# Print out result
+print(result)
+
+savings = 100
+# Print out savings
+print(savings)
+
+print(10 + 7)
